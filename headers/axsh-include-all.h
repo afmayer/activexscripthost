46e2d515f0c36c3cd55b4023789c627fb41e0337
Alexander F. Mayer <tinybluebutton@gmail.com>
2012-06-16 01:16:14 +0200

    Renamed axsh-string-to-guid.c/.h to axsh-string-guid-util.c/.h
    Signed-off-by: Alexander F. Mayer <tinybluebutton@gmail.com>

--- headers/axsh-include-all.h
+++ headers/axsh-include-all.h
@@ -14,7 +14,7 @@
 #include "axsh-tcl-engine-subcommands.h"
 #include "axsh-engine-interact.h"
 #include "axsh-activescriptsite-impl.h"
-#include "axsh-string-to-guid.h"
+#include "axsh-string-guid-util.h"
 #include "axsh-misc-defines.h"
 #include "axsh-hostcontrol-impl.h"
 
